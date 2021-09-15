ad0a920d6b07807cbfe30cacbe32a59ae0975a69
RakeshThakur26 <rakesh.thakur.mail99@gmail.com>
2021-09-15 16:08:13 +0530

    Day 3 Task on File handling

---
+++ Day  task/FileHandling.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Day__task
+{
+    class FileHandling
+    {
+        public void CreateFile()
+        {
+            FileStream fs = new FileStream(@"C:\Training\Task\Day  task\Day  task\mytest.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            Console.WriteLine("File created successfully");
+        }
+
+        public void DeleteFile()
+        {
+            File.Delete(@"C:\Training\Task\Day  task\Day  task\mytest.txt");
+            Console.WriteLine("File deleted Successfully");
+        }
+
+        public void IsExist()
+        {
+            if (File.Exists(@"C:\Training\Task\Day  task\Day  task\mytest.txt"))
+                Console.WriteLine("File already exists..!");
+            else
+            {
+                Console.WriteLine("Redirecting to Create file...");
+                CreateFile();
+            }
+            
+        }
+
+        public void WriteFile()
+        {
+            FileStream fs = new FileStream(@"C:\Training\Task\Day  task\Day  task\mytest.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            int i = 1;
+            sw.WriteLine(i++ + "Hello and welcome");
+            sw.WriteLine(i++ + "This is the Second line");
+            sw.WriteLine(i++ + "This is Third line of mytest.txt");
+
+            sw.Close();
+            fs.Close();
+            Console.WriteLine("Data entered successfully");
+
+        }
+
+        public void ReadFile()
+        {
+            FileStream fs = new FileStream(@"C:\Training\Task\Day  task\Day  task\mytest.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string line = sr.ReadLine();
+           
+            while (sr.Peek() > 0) {
+                Console.WriteLine(line);
+                line = sr.ReadLine();   
+            }
+
+        }
+        public void ReadPerticularLine(int n)
+        {
+            FileStream fs = new FileStream(@"C:\Training\Task\Day  task\Day  task\LineText.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+           
+            int i = 1;
+            while (i<n)
+            {
+                string line = sr.ReadLine();
+                i++;
+            }
+            Console.WriteLine(sr.ReadLine());
+
+        }
+
+        public void TotalLines()
+        {
+            FileStream fs = new FileStream(@"C:\Training\Task\Day  task\Day  task\Lines.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string line = sr.ReadLine();
+
+            int i = 1;
+
+            while (sr.Peek() > 0)
+            {
+                line = sr.ReadLine();
+                i++;
+            }
+
+            Console.WriteLine("Total number of lines = " + i);
+        }
+    }
+}
---
+++ Day  task/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day__task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileHandling file = new FileHandling();
+            // 1. Creating a new file mytest.txt
+
+            // file.DeleteFile();
+
+
+            //2. Deleteing file mytest.txt
+
+            // file.DeleteFile();
+
+            // 3. Is file already exist?
+            //file.IsExist();
+
+            //4. Creating a new file mytest.txt and writing into it
+
+            //file.WriteFile();
+
+            // 5. Reading contents from the file
+
+            //file.ReadFile();
+
+            //6. Display the perticular line data from the file
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            file.ReadPerticularLine(n);
+
+
+            //7. Counting the total number of lines
+            //file.TotalLines();
+
+            Console.Read(); 
+        }
+    }
+}
